ab53d40327bd93764a6d13a658cc4513c1a45d60
Total-Hydrographic <55672165+Total-Hydrographic@users.noreply.github.com>
2019-09-26 13:18:55 +1000

    test

---
+++ pointclouds/testpage/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 396009,
+    "boundingBox": {
+        "lx": 799777.2609999999,
+        "ly": 5871663.29,
+        "lz": 452.71,
+        "ux": 799815.0300000003,
+        "uy": 5871701.059,
+        "uz": 490.47900000032038
+    },
+    "tightBoundingBox": {
+        "lx": 799777.2609999999,
+        "ly": 5871663.29,
+        "lz": 452.71,
+        "ux": 799811.85,
+        "uy": 5871701.059,
+        "uz": 455.129
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.3270891308784485,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
